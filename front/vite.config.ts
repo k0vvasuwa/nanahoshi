e6133f9af5519d9385bee0e941c7c2f7113876fe
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-25 21:25:28 +0500

    Add #enums alias

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
         ],
         alias: {
             '#styles': '/src/styles.css',
+            '#enums': '/src/enums.ts',
             '#types': '/src/types.ts',
             '#router': '/src/router.ts',
             '#functions': '/src/functions',
