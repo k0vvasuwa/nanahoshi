02a6689fe87e95ac89cd522e6b5cd3f6e4aadab4
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-25 19:54:18 +0500

    Add set_csrf_token() URL to urlpatterns

--- nanahoshi/urls.py
+++ nanahoshi/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 
 from django.contrib import admin
 
+from .views import set_csrf_token
+
+
+api_prefix: str = 'api/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(f'{api_prefix}set-csrf-token', set_csrf_token)
 ]
