cd54ac53393de7009b33695999f294181eb97318
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 00:12:30 +0500

    Create Settings model

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,15 @@
+from typing import (
+    List,
+    Dict
+)
+
 from django.db import models
 
 
+class Settings(models.Model):
+    dark_theme: bool = models.BooleanField(default=False)
+    expanded_notes: List[int] = models.JSONField(default=list)
+    opened_tabs: List[int] = models.JSONField(default=list)
+    splitter_size: int = models.PositiveIntegerField(default=20)
+    programming_languages: Dict[str, str] = models.JSONField(default=dict)
+    
