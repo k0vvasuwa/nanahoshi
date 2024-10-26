0f97971a57852b704d0d6f5128801e132529dcd6
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-26 23:52:07 +0500

    Add #store alias

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
         ],
         alias: {
             '#styles': '/src/styles.css',
+            '#store': '/src/store.ts',
             '#enums': '/src/enums.ts',
             '#types': '/src/types.ts',
             '#router': '/src/router.ts',
