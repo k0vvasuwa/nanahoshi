e425f0bbe6df4e3db6f674a69cbd55323c07b449
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 15:06:16 +0500

    Register Note model

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Note
 
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'position',
+        'parent',
+        'has_children'
+    ]
