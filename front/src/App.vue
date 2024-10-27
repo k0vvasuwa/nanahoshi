cc0de7595acf391ee31453fd3e04151eb5b96fa1
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 11:33:09 +0500

    Create toast object

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,13 +8,45 @@ import {
     useRoute
 } from 'vue-router';
 
-import useSettingsStore from './store.ts';
+import { ToastServiceMethods } from 'primevue/toastservice';
+import { useToast } from 'primevue/usetoast';
+import Toast from 'primevue/toast';
+import ConfirmDialog from 'primevue/confirmdialog';
+
+import useSettingsStore from '#store';
+
+import { Severity } from '#enums';
 
 import { setCsrfToken } from '#functions/requests';
 
 
 
 const settings = useSettingsStore();
+
+const toastService: ToastServiceMethods = useToast();
+
+const toast = {
+    show(type: Severity, message: string, details?: string): void {
+        toastService.add({
+            severity: type,
+            summary: message,
+            detail: details,
+            life: 10_000
+        });
+    },
+    success(message: string, details?: string): void {
+        this.show(Severity.Success, message, details);
+    },
+    info(message: string, details?: string): void {
+        this.show(Severity.Info, message, details);
+    },
+    warn(message: string, details?: string): void {
+        this.show(Severity.Warn, message, details);
+    },
+    error(message: string, details?: string): void {
+        this.show(Severity.Error, message, details);
+    }
+};
 
 const router: Router = useRouter();
 const route: RouteLocationNormalizedLoaded = useRoute();
@@ -48,6 +80,7 @@ async function start(): Promise<void> {
 
 start();
 
+provide('toast', toast);
 provide('redirect', redirect);
 </script>
 
