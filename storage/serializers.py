de6cf72956eb68948cba1234e60d372c3807d72e
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 14:11:15 +0500

    Create NoteSerializer

--- storage/serializers.py
+++ storage/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from .models import Settings
+from .models import (
+    Settings,
+    Note
+)
 
 
 class SettingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Settings
+        fields = '__all__'
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
         fields = '__all__'
