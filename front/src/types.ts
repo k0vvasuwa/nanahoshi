04a9e854cef5ababa57ec2529c7208e4fb60aed7
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 11:31:50 +0500

    Create Settings type

--- front/src/types.ts
+++ front/src/types.ts
@@ -1,1 +1,11 @@
+export type Settings = {
+    dark_theme: boolean
+    expanded_notes: number[]
+    opened_tabs: number[]
+    splitter_size: number
+    programming_languages: object
+}
+
+
+
 export type Redirect = (url: string) => Promise<void>
