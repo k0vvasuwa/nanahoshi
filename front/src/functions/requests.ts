b14cc78760fe0bdb630d1e60b11eed5e9a5f885d
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 11:37:54 +0500

    Remove auth requests

--- front/src/functions/requests.ts
+++ front/src/functions/requests.ts
@@ -19,14 +19,3 @@ export async function setCsrfToken(): Promise<void> {
     await axios.get(`${apiPrefix}set-csrf-token`);
     axiosDefaults.headers['X-CSRFToken'] = getCsrfToken();
 }
-
-export async function login(username: string, password: string): Promise<void> {
-    await axios.post(`${authUrl}login`, {
-        username,
-        password
-    });
-}
-
-export async function logout(): Promise<void> {
-    await axios.get(`${authUrl}logout`);
-}
