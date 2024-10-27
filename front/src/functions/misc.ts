95eaefb3b1f6b1986d20bf6b4c3a4b2d081ffb5d
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 14:15:31 +0500

    Create getRootNote() function

--- front/src/functions/misc.ts
+++ front/src/functions/misc.ts
@@ -1,5 +1,7 @@
 import Cookies from 'js-cookie';
 
+import { Note } from '#types';
+
 
 
 export function getCsrfToken(): string {
@@ -12,3 +14,12 @@ export function getCsrfToken(): string {
     return token;
 }
 
+export function getRootNote(): Note {
+    return {
+        id: 1,
+        name: 'Конспектики',
+        position: 1,
+        parent: null as unknown as number,
+        has_children: true
+    };
+}
