e6159a2bab680aa0677d8746df07e4785cb268c1
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 15:06:32 +0500

    Set up node slot

--- front/src/components/Reference.vue
+++ front/src/components/Reference.vue
@@ -5,10 +5,19 @@ import {
     onMounted
 } from 'vue';
 
-import { Draggable } from '@he-tree/vue'
-import '@he-tree/vue/style/default.css'
+import { Draggable } from '@he-tree/vue';
+import { Stat } from '@he-tree/tree-utils';
+import '@he-tree/vue/style/default.css';
+import '@he-tree/vue/style/material-design.css';
 
 
+import { Note } from '#types';
+
+import { getRootNote } from '#functions/misc';
+
+
+
+const notes = ref<Note[]>([getRootNote()]);
 
 const tree = useTemplateRef<InstanceType<typeof Draggable>>('tree');
 const height = ref<string>('800px');
@@ -22,22 +31,43 @@ function calcTreeHeight(): void {
     height.value = `${docHeight - offset}px`;
 }
 
+function expandNode(event: PointerEvent, stat: Stat<Note>): void {
+    (event.target as HTMLElement).classList.toggle('down');
+    stat.open = !stat.open;
+}
+
 
 
 onMounted((): void => {
+    tree.value!.$el.querySelector('i').classList.toggle('down');
     calcTreeHeight();
     window.addEventListener('resize', calcTreeHeight);
 });
 </script>
 
 <template>
-    <Draggable id="tree" ref="tree" textKey="name">
-
+    <Draggable id="tree" class="mtl-tree" ref="tree" v-model="notes" virtualization textKey="name">
+        <template #default="{ node: note, stat}" >
+            <i class="pi pi-angle-right" @click="expandNode($event as PointerEvent, stat)" />
+            <div class="spacer">{{ note.name }}</div>
+        </template>
     </Draggable>
 </template>
 
 <style scoped>
 #tree {
     height: v-bind(height);
+}
+
+:deep(.tree-node-inner:hover) {
+    background-color: var(--expand-icon-hover-background);
+}
+
+.pi-angle-right {
+    transition: rotate 150ms;
+}
+
+.down {
+    rotate: 90deg;
 }
 </style>
