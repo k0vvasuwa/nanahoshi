3f17405c372b3f24ed5f35ed07763056cc78c3ad
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 13:55:42 +0500

    Place Splitter

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
 
+import Button from 'primevue/button';
+
+import useSettingsStore from '#store';
+
+
+
+const settings = useSettingsStore();
 </script>
 
 <template>
-
+    <Splitter stateKey="splitterSize" stateStorage="local">
+        <SplitterPanel :minSize="20">
+            <div class="flex-row align-center">
+                <div class="app-logo">Nanahoshi</div>
+                <div class="spacer"></div>
+                <Button :icon="settings.darkTheme ? 'pi pi-moon' : 'pi pi-sun'" text rounded
+                        @click="settings.toggleTheme(true)" />
+                <Button icon="pi pi-cog" text rounded />
+            </div>
+        </SplitterPanel>
+        <SplitterPanel :minSize="60">
+            <div>5</div>
+        </SplitterPanel>
+    </Splitter>
 </template>
 
 <style scoped>
