2a42bb665b567190dfdbf07c53598f95cfe7c3b9
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-27 14:12:47 +0500

    Add NoteViewSet URLs to urlpatterns

--- storage/urls.py
+++ storage/urls.py
@@ -5,11 +5,15 @@ from django.urls import (
 
 from rest_framework import routers
 
-from .views import SettingsViewSet
+from .views import (
+    SettingsViewSet,
+    NoteViewSet
+)
 
 
 router = routers.DefaultRouter()
 router.register(r'settings', SettingsViewSet)
+router.register(r'notes', NoteViewSet, 'notes')
 
 urlpatterns = [
     path('', include(router.urls))
