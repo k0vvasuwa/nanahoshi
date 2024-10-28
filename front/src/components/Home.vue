50e79b561ed1099555cbd4daea831d84c70b34d0
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-28 16:50:00 +0500

    Implement menu

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,10 +1,21 @@
 <script setup lang="ts">
+import {
+    ref,
+    useTemplateRef,
+    inject
+} from 'vue';
+
 import Splitter from 'primevue/splitter';
 import SplitterPanel from 'primevue/splitterpanel';
 
 import Button from 'primevue/button';
 
+import { MenuItem } from 'primevue/menuitem';
+import Menu from 'primevue/menu';
+
 import useSettingsStore from '#store';
+
+import { Redirect } from '#types';
 
 import Reference from '#components/Reference';
 import NotePage from '#components/NotePage';
@@ -12,6 +23,30 @@ import NotePage from '#components/NotePage';
 
 
 const settings = useSettingsStore();
+
+const redirect = inject('redirect') as Redirect;
+
+const menu = useTemplateRef<InstanceType<typeof Menu>>('menu');
+const menuItems = ref<MenuItem[]>(getMenuItems());
+
+
+
+function getMenuItems(): MenuItem[] {
+    return [
+        {
+            label: 'Языки программирования',
+            icon: 'pi pi-code'
+        },
+        {
+            label: 'Выйти',
+            icon: 'pi pi-sign-out',
+            async command(): Promise<void> {
+                await settings.logout();
+                await redirect('/login');
+            }
+        }
+    ];
+}
 </script>
 
 <template>
@@ -22,7 +57,8 @@ const settings = useSettingsStore();
                 <div class="spacer"></div>
                 <Button :icon="settings.darkTheme ? 'pi pi-moon' : 'pi pi-sun'" text rounded
                         @click="settings.toggleTheme(true)" />
-                <Button icon="pi pi-cog" text rounded />
+                <Button icon="pi pi-cog" text rounded @click="menu?.toggle"/>
+                <Menu ref="menu" :model="menuItems" popup />
             </div>
             <Reference />
         </SplitterPanel>
