a6f4580293d665ace7ae6d09cda13b255a0934b7
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-28 22:35:13 +0500

    Start work on component

--- front/src/components/NotePage.vue
+++ front/src/components/NotePage.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import {
+    ref
+} from 'vue';
 
+import {
+    getNotePage
+} from '#functions/requests';
+
+
+
+const props = defineProps<{
+    id: number,
+    title: string
+}>();
+
+
+
+function loadPage(): void {
+
+}
+
+
+
+loadPage();
 </script>
 
 <template>
-
+    <div>{{ title }}</div>
 </template>
 
 <style scoped>
