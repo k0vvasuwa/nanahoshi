518ab78246be378b92a7107d00d86ac6f3aede52
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-29 20:02:09 +0500

    Add URL for index.html

--- nanahoshi/urls.py
+++ nanahoshi/urls.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from django.urls import (
     path,
+    re_path,
     include
 )
 
@@ -23,3 +25,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns.append(re_path(".*", TemplateView.as_view(template_name="index.html")))
