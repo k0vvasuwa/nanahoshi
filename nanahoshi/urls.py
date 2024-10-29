e5a4accf4f4838ec4fe98428e7c6b83719b606b4
k0vvasuwa <k0vvasuwa@yandex.ru>
2024-10-29 18:07:27 +0500

    Add media and static URLs

--- nanahoshi/urls.py
+++ nanahoshi/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import (
     path,
     include
@@ -16,3 +19,7 @@ urlpatterns = [
     path(f'{api_prefix}auth/', include('authentication.urls')),
     path(f'{api_prefix}storage/', include('storage.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
